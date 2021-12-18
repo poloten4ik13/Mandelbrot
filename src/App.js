c9d95b02994826c9c3ae73936b019dbf2321840e
poloten4ik13 <cocobill13@rambler.ru>
2021-12-18 03:43:26 +0200

    add zoom without change canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Canvas from './Canvas'
+
+
+  const App = ()=> {
+    return <Canvas  width={1600} height={800}/>
 }
 
 export default App;
